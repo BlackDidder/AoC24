6c5959dd29fa878a666c2b311d211747d491df96
BlackDidder <189620408+BlackDidder@users.noreply.github.com>
2024-12-02 23:47:10 +0100

    Day One Task One

---
+++ Day1/Task1.py
@@ -0,0 +1,15 @@
+with open("/home/simon/IdeaProjects/AoC24/input.txt", "r") as f:
+    file=f.read()
+lines=file.splitlines()
+leftN = []
+rightN = []
+for line in lines:
+    left, right = map(int, line.split())
+    leftN.append(left)
+    rightN.append(right)
+leftN.sort()
+rightN.sort()
+d=0
+for left, right in zip(leftN, rightN):
+    d+=abs(left-right)
+print(d)
