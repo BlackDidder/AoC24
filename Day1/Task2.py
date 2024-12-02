1c50dd5eb2c0565916de2a43a8e807e5256f801c
BlackDidder <189620408+BlackDidder@users.noreply.github.com>
2024-12-02 23:59:29 +0100

    Day One Task Two

---
+++ Day1/Task2.py
@@ -0,0 +1,15 @@
+with open("/home/user/IdeaProjects/AoC24/input.txt", "r") as f:
+    file=f.read()
+lines=file.splitlines()
+leftN = []
+rightN = []
+for line in lines:
+    left, right = map(int, line.split())
+    leftN.append(left)
+    rightN.append(right)
+leftN.sort()
+rightN.sort()
+n=0
+for num in leftN:
+    n+=num*rightN.count(num)
+print(n)
