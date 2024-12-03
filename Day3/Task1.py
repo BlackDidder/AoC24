5562c04fafca39e3fa78308fe98a32add7203065
BlackDidder <189620408+BlackDidder@users.noreply.github.com>
2024-12-03 22:10:09 +0100

    Day Three Task One

--- Day3/Task1.py
+++ Day3/Task1.py
@@ -1,8 +1,8 @@
-with open("/home/user/IdeaProjects/AoC24/Day2/input", "r") as f:
+import re
+with open("/home/simon/IdeaProjects/AoC24/Day3/input", "r") as f:
     file = f.read()
-allMul = []
-k=0
-
-for i in range(1000):
-    for j in range(1000):
-        allMul.append("mul("+str(i)+","+str(j)+")")
+mul = 0
+x = re.findall(r"mul\(([0-9]*),([0-9]*)\)",file)
+for i in x:
+    mul += (int(i[0])*int(i[1]))
+print(mul)
