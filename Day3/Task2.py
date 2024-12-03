ee77556b0dc4a0b3387422cd21b00b7f5c87de5d
BlackDidder <189620408+BlackDidder@users.noreply.github.com>
2024-12-03 23:50:47 +0100

    Day Three Task Two

---
+++ Day3/Task2.py
@@ -0,0 +1,14 @@
+import re
+with open("/home/simon/IdeaProjects/AoC24/Day3/input", "r") as f:
+    file = f.read()
+mul = 0
+enabled = True
+x = re.findall(r"(do\(\))|(don't\(\))|mul\(([0-9]*),([0-9]*)\)", file)
+for i in x:
+    if i[0] == "do()":
+        enabled = True
+    elif i[1] == "don't()":
+        enabled = False
+    elif enabled:
+        mul += (int(i[2])*int(i[3]))
+print(mul)
